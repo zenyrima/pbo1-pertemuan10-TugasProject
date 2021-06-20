3837dd89aec936beada24be6061c01362acda4d1
zenyrima <rimadamayanti69@gmail.com>
2021-06-20 17:25:07 +0800

    class member dengan 3 atribut dan 2 method

---
+++ src/model/member.java
@@ -0,0 +1,42 @@
+package model;
+import java.time.*;
+public class member {
+    // atribut tambahan untuk member
+    String id_member;
+    String No_member;
+    String masa_berlaku;
+
+    // constructor
+
+
+    public member(String id_member, String no_member, String masa_berlaku) {
+        this.id_member = id_member;
+        this.No_member = no_member;
+        this.masa_berlaku = masa_berlaku;
+    }
+
+    // menghitung member
+    long hitungmember() {
+        long member = 30000;
+        return member;
+    }
+
+    // menghitung voucher tambahan
+    long hitungvoucher() {
+        long hitungvoucher;
+        if (this.hitungvoucher() >= 100000) {
+            hitungvoucher = 20000;
+        } else {
+            hitungvoucher = 0;
+        }
+
+        return hitungvoucher;
+    }
+
+    // cetak data member
+    void printmember() {
+        System.out.println("ID Member   : " + this.id_member);
+        System.out.println("No Member : " + this.No_member);
+        System.out.println("Masa Berlaku     : " + this.masa_berlaku);
+    }
+}
