58637d6626c88c54af9f5dbfc79fcbb77e36d911
zenyrima <rimadamayanti69@gmail.com>
2021-06-21 10:39:00 +0800

    class main buat 2 object untuk tiap constructor class pengunjung

--- src/model/main.java
+++ src/model/main.java
@@ -7,10 +7,21 @@ public class main {
                 "ANDI LUKMANA", 30000, 400000, 2, 1,
                 1000);
         p1.printPengunjung();
+        pengunjung p3 = new pengunjung("Jl.Biman Villa gang bhakti Rt 01", "081334251679");
+        pengunjung p4 = new pengunjung();
+
+        pengunjung p5 = new pengunjung("RISKA", 12, 14,
+                "0231", "WOR-W/HST01/21891", "DA1761", "MATIC",
+                "MAIDI", 30000, 500000, 1, 1,
+                1000);
+        p5.printPengunjung();
+        pengunjung p6= new pengunjung("Jl.soekarno hatta", "081233251679");
+        pengunjung p7 = new pengunjung();
         // contoh obyek data member
     member p2= new member("anjanni", 2, 4, "028", "876",
             "DA9870", "matic", "AHMAD SAIDI", 20000, 750000,
             2, 1, 1000, "0918", "0981", "S/D 2024");
     p2.printmember();
+
     }
 }
